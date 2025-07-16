227ae4dcc6544301de168fc41a654187748c92e4
Gleb Krasnopolin <gleb.krasnopolin@umain.com>
2025-07-17 01:57:19 +0200

    Initial Commit

---
+++ testproduct Watch App/testproductApp.swift
@@ -0,0 +1,17 @@
+//
+//  testproductApp.swift
+//  testproduct Watch App
+//
+//  Created by Gleb Krasnopolin on 2025-07-17.
+//
+
+import SwiftUI
+
+@main
+struct testproduct_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
