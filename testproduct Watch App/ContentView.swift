8f8352bd3bebcaf200d48bab31dec4876b94f9bb
Gleb Krasnopolin <gleb.krasnopolin@umain.com>
2025-08-01 17:50:27 +0200

    full application

--- testproduct Watch App/ContentView.swift
+++ testproduct Watch App/ContentView.swift
@@ -1,23 +1,60 @@
 //
 //  ContentView.swift
-//  testproduct Watch App
+//  beertime Watch App
 //
-//  Created by Gleb Krasnopolin on 2025-07-17.
+//  Created by Gleb Krasnopolin on 2025-07-15.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct GIFView: View {
+    let frameCount = 33
+    @State private var currentFrame = 1
+    let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image("\(currentFrame)")
+            .resizable()
+            .scaledToFit()
+            .onReceive(timer) { _ in
+                currentFrame = (currentFrame %  frameCount) + 1
+            }
     }
 }
+
+struct ContentView: View {
+    
+    @State private var currentTime = Date()
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+            VStack(spacing: 1) {
+                Text("It's already")
+                    .font(.custom("Pirata One", size: 20))
+                    .bold()
+                
+                Text("\(formattedTime)")
+                    .font(.custom("Pirata One", size: 50))
+                
+                Text("time to drink:")
+                    .font(.custom("Pirata One", size: 20))
+                    .foregroundColor(.yellow)
+                
+                GIFView()
+            }._statusBarHidden()
+            .onReceive(timer) { time in
+                currentTime = time
+            }
+    }
+        
+    
+    var formattedTime: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter.string(from: currentTime)
+    }
+}
+
 
 #Preview {
     ContentView()
